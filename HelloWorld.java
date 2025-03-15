69dde152283ed52bff505f3736c5d11dcbdc3a4a
MGarza9823 <garza.m980123@stu.sanjac.edu>
2025-03-15 02:28:38 +0000

    Pending changes exported from your codespace

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+public class HelloWorld 
+{
+    public static void main(String [] angs)
+    {
+        /* The main() method represents the start of the execution of the program */
+        /* The system.out.println() is used to display output because it convinietly prints text into the console */
+        /* The purpose of comments in the program is to provide a better understanding of the code */
+        System.out.println("Hello World!");
+        // Displays Hello World!
+        System.out.println("Mariana Garza");
+        // Displays Name
+        System.out.println("Barbecue");
+        // Displays Favorite food
+        System.out.println("Dolly Parton");
+        // Dsiplays public figure I would like to meet
+        System.out.println("Folding Clothes");
+        // Displays least favorite chore at home
+        System.out.println("Playing Video games");
+        // Displays what I would rather be doing than completing this lab assignment
+    }
+    
+    
+}
